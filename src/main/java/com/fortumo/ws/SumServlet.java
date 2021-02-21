b9637532aed1071dd39d33a50a7fe2cae5757c8e
mboysan <dev@mboysan.com>
2021-02-21 22:47:47 +0200

    refs #8 - try to satisfy static code analyzer (2) - added Javadocs for SumServlet

--- src/main/java/com/fortumo/ws/SumServlet.java
+++ src/main/java/com/fortumo/ws/SumServlet.java
@@ -12,6 +12,11 @@ import java.io.IOException;
 import static jakarta.servlet.http.HttpServletResponse.SC_BAD_REQUEST;
 import static jakarta.servlet.http.HttpServletResponse.SC_INTERNAL_SERVER_ERROR;
 
+/**
+ * The servlet class that handles the requests received by the application server. Note that the number of concurrent
+ * requests that this servlet handles depends on the number of connections the backing application server is
+ * configured with.
+ */
 public class SumServlet extends HttpServlet {
 
     private static final Logger LOG = LoggerFactory.getLogger(SumServlet.class);
@@ -25,10 +30,22 @@ public class SumServlet extends HttpServlet {
         this.service = createService();
     }
 
+    /**
+     * VisibleForTesting.
+     * @return the service object.
+     */
     SumService createService() {
         return new SumService();
     }
 
+    /**
+     * Handles the POST:/ request with body containing a number or the string 'end'. After validating the request body,
+     * it is sent to the {@link #service} for processing.
+     *
+     * @param req request object to receive the client's request.
+     * @param resp response object to send response.
+     * @throws IOException if I/O stream cannot be processed correctly.
+     */
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         try {
@@ -54,7 +71,11 @@ public class SumServlet extends HttpServlet {
         }
     }
 
-    void validate(String body) {
+    /**
+     * Validates the request body.
+     * @param body the request body to validate.
+     */
+    private void validate(String body) {
         if (body == null || body.isBlank()) {
             throw new IllegalArgumentException("body must not be null or blank");
         }
